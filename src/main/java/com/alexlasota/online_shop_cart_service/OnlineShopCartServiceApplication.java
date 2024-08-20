bfbaffee3e769a5dd36ef1da542ecbcf47abf464
alexlasota <alexylasota@gmail.com>
2024-08-20 20:53:18 +0200

    hi

---
+++ src/main/java/com/alexlasota/online_shop_cart_service/OnlineShopCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alexlasota.online_shop_cart_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopCartServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexlasota/online_shop_cart_service/OnlineShopCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexlasota.online_shop_cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
